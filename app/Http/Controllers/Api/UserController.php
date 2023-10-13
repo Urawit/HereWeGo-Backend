c54ecb5527105cec1bd0d67558f684ee83aff67a
Urawit <urawit.j@ku.th>
2023-10-13 21:22:57 +0700

    feat: initialize Authorization with JWT, Register Login Logout done

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function getAllUsers()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Users retrieved successfully',
+            'users' => $users
+        ]);
+    }
+    // /**
+    //  * Show the form for editing the user settings.
+    //  */
+    // public function settings()
+    // {
+    //     $user = User::find(Auth::id());
+    //     // split the name into first and last
+    //     $name = explode(' ', $user->name, 2);
+    //     $first_name = $name[0];
+    //     $last_name = $name[1];
+    //     return view('settings', compact('user', 'first_name', 'last_name'));
+    // }
+
+    // public function update(Request $request, User $user)
+    // {
+    //     $request->validate([
+    //         'first_name' =>  ['required', 'string', 'min:1','max:255'],
+    //         'last_name' => ['required', 'string', 'min:1','max:255'],
+    //         'socials' => ['nullable', 'string', 'max:255'],
+    //         // 'email' => 'required|email|max:255|unique:users,email,' . $user->id, // This line enforces unique emails and ignores the current user
+    //         'profile' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg','max:2048'],
+    //         // 'certificates' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg' ,'max:2048'],
+    //         'about' => ['nullable', 'string']
+    //     ]);
+
+    //     $user = User::find(Auth::id());
+        
+    //     // Check if user uploading a file, if uploaded store in storage
+    //     if ($request->hasFile('profile')) {
+    //         $file = $request->file('profile');
+    //         $path = $file->storeAs(
+    //             'public/avatars',
+    //             $user->id . '.' . $file->getClientOriginalExtension()
+    //         );
+    //         $filePath = str_replace('public/', '', $path);
+
+    //         // Update avatar field in user model
+    //         $user->avatar = $filePath;
+    //     }
+        
+    //     // Check if user uploading a file, if uploaded certificates and store in storage
+    //     if ($request->hasFile('certificates')) {
+    //         $file = $request->file('certificates');
+    //         $filePath = $request->file('certificates')->store('certificates', 'public');
+
+    //         // Update certificates field in user model
+    //         $user->certificate = $filePath;
+    //     }
+        
+    //     // update other fields
+    //     $user->name = $request->input('first_name') . ' ' . $request->input('last_name');
+    //     // $user->email = Auth::user()->email;
+    //     $user->social = $request->input('socials');
+    //     $user->about = $request->get('about');
+    //     $user->save();
+    //     return redirect()->back()->with('status', 'updated');
+    // }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/users', [UserController::class, 'getAllUsers']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
