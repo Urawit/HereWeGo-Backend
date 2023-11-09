a5120bf9bdb772f038a59a2162da565a02e6f483
Urawit <urawit.j@ku.th>
2023-11-09 10:03:21 +0700

    fix: seeder

--- database/factories/FavoriteFactory.php
+++ database/factories/FavoriteFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\Activity;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Favorite; // Assuming Favorite is the model being used
 
 class FavoriteFactory extends Factory
 {
@@ -16,9 +16,21 @@ class FavoriteFactory extends Factory
      */
     public function definition(): array
     {
+        do {
+            $randomUserId = User::inRandomOrder()->first()->id;
+            $randomActivityId = Activity::inRandomOrder()->first()->id;
+        } while ($this->checkDuplicate($randomUserId, $randomActivityId));
+
         return [
-            'user_id' => User::all()->random()->id,
-            'activity_id' => Activity::all()->random()->id,
+            'user_id' => $randomUserId,
+            'activity_id' => $randomActivityId,
         ];
+    }
+
+    private function checkDuplicate($userId, $activityId): bool
+    {
+        return Favorite::where('user_id', $userId)
+            ->where('activity_id', $activityId)
+            ->exists();
     }
 }
